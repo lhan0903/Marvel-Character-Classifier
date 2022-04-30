f7731240a99a989d3fc1b3993c2d9ee8896367b5
Linda Han <hanlinda0903@gmail.com>
2022-04-30 13:05:11 -0700

    ready to build the website

--- server/server.py
+++ server/server.py
@@ -1,18 +1,23 @@
 from flask import Flask, request, jsonify
+import util
 
 app  = Flask(__name__)
 
 # # runs a server on 5000 port and supports an API called hello which returns "hi"
-# @app.route('/classify_image', methods = ['GET', 'POST'])
-# def classify_image():
-#     return "hi"
+@app.route('/classify_image', methods = ['GET', 'POST'])
+def classify_image():
+    image_data = request.form['image_data']
 
-@app.route('/hello/<name>')
-def hello(name):
-    return "HI" + name
+    response = jsonify(util.classify_image(image_data))
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 
 if __name__ == "__main__":
+    print("Staring Python Flask Server For Avenger Image Classification")
+    util.load_saved_artifacts()
     app.run(port=5000)
 
 # Running the program above outputs "Running on http://127.0.0.1:5000" 
